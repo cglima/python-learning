0645b23ee35ff8d121d3e6fe8667175aabb536aa
Ciana Lima <lima.cglima@gmail.com>
2019-12-28 22:38:11 +0000

    aprendendo variação da função print

---
+++ ciana/funcaoprint.py
@@ -0,0 +1,21 @@
+print("Hello 1+2")
+
+print(f'Hello {1+2}')
+
+nome = "jom"
+print(f'Hello {nome}')
+
+idade = 1
+#camelcase 
+eMaiorDeIdade = idade >= 18
+
+#operador condicional
+print(f'é maior de idade: {"Sim" if eMaiorDeIdade else "Nao"}')
+
+
+idade = 29 
+eMenorDeIdade = idade <= 18
+
+print(f'É MENOR DE IDADE: {"sim" if eMenorDeIdade else "não"}')
+
+
